87395d68de61b2c3d4b0b75f6281c88f8f355598
Philip Turner <philipturner.AR@gmail.com>
2022-07-03 19:53:28 -0400

    Initial commit

---
+++ Tests/AutoDiffPackage2Tests/AutoDiffPackage2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AutoDiffPackage2
+
+final class AutoDiffPackage2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AutoDiffPackage2().text, "Hello, World!")
+    }
+}
