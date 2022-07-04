bc17ab6d59e2806dee47f717bdd3e1d1056d4c15
Philip Turner <philipturner.AR@gmail.com>
2022-07-04 11:18:23 -0400

    narrow bug 4

--- Package.swift
+++ Package.swift
@@ -5,27 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "AutoDiffPackage2",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "AutoDiffPackage2",
-            targets: ["AutoDiffPackage2"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/philipturner/differentiation", .branch("main")),
-    ],
+    products: [],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AutoDiffPackage2",
-            dependencies: [
-                .product(name: "_Differentiation", package: "differentiation"),
-            ]),
+      .target(
+        name: "Differentiation"),
         .testTarget(
             name: "AutoDiffPackage2Tests",
-            dependencies: ["AutoDiffPackage2"]),
+            dependencies: ["Differentiation"]),
     ]
 )
